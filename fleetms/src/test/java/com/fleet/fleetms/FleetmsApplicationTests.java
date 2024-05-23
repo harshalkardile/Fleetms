00aaab5a54e0307a18ebf44b534843b2f0d058f4
harshal <kardileharshal567@gmail.com>
2024-05-24 00:58:25 +0530

    setup commit

---
+++ fleetms/src/test/java/com/fleet/fleetms/FleetmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleet.fleetms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
