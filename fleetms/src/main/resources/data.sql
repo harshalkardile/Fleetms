6f5f65a8109106301f257f06dadbb9abcb72e90f
harshal <kardileharshal567@gmail.com>
2024-05-28 22:22:30 +0530

    added model classes for with database configuration

---
+++ fleetms/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into Country value (1, 'Washington DC', 'USA', 'North America', 'United State', 'American');
+insert into Country value (2, 'Abuja', 'NGN', 'North America', 'United State', 'American');
+insert into Country value (3, 'New Delhi', 'IN', 'North America', 'United State', 'American');
+insert into Country value (4, 'Budapest', 'HUN', 'North America', 'United State', 'American');
