0d9ea8b267281e93f31f368a83795d617d6ffa9d
harshal <kardileharshal567@gmail.com>
2024-07-15 15:14:07 +0530

    completed supplier module and fixed bugs of fields

---
+++ fleetms/src/main/java/com/fleet/fleetms/parameters/repositories/SupplierRepository.java
@@ -0,0 +1,10 @@
+package com.fleet.fleetms.parameters.repositories;
+
+import com.fleet.fleetms.parameters.models.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+
+}
