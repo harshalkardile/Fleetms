d156d04f2be4227ce8e91cc3e3f0db468cf104fd
harshalk <kardileharshal567@gmail.com>
2024-10-14 21:26:29 +0530

    updated model import

--- fleetms/src/main/java/com/fleet/fleetms/reports/ReportsController.java
+++ fleetms/src/main/java/com/fleet/fleetms/reports/ReportsController.java
@@ -5,6 +5,7 @@ import com.fleet.fleetms.hr.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class ReportsController {
